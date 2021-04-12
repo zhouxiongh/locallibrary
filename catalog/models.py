759d5b6d70e22f18cadd5a4590bf8a0f97f3244f
hezhoux <hezhoux@qq.com>
2021-04-12 16:49:43 +0800

    feat: list borrowed books

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,9 @@
+from datetime import date
+
 from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL
 import uuid
+from django.contrib.auth.models import User
 
 
 # patterns
@@ -78,6 +81,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -91,12 +96,19 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """
         String for representing the Model object
         """
         return '%s (%s)' % (self.id, self.book.title)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,8 @@ from django.views import generic
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def index(request):
     """
@@ -51,3 +53,16 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(
+            status__exact='o').order_by('due_back')
