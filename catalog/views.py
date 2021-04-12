2ea777aabe54a3ff82e4d33dc76f269e140f9e62
hezhoux <hezhoux@qq.com>
2021-04-12 14:16:48 +0800

    feat: author detail view and list view

--- catalog/views.py
+++ catalog/views.py
@@ -17,13 +17,17 @@ def index(request):
         status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available,
-                 'num_authors': num_authors},
+                 'num_authors': num_authors,
+                 'num_visits': num_visits},
     )
 
 
