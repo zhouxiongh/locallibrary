147811b6fe34d47c730ba01df67cb074e5956eee
hezhoux <hezhoux@qq.com>
2021-04-13 11:03:03 +0800

    test: forms models views

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
 urlpatterns += [
